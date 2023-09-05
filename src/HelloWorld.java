27dc6217bf430f4d58200ee57629c2094885118c
AlexSoulJk <alex.soloamtow2016@yandex.ru>
2023-09-05 23:48:22 +0300

    Named Sanya

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
+    public static void print_sanya(){
+        System.out.println("Sanya pidor-ass xD");
+    }
+
     public static void main(String[] args){
         System.out.println("Hello, world!");
+        print_sanya();
     }
 }
